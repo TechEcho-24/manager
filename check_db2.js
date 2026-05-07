bcdec45ba5355ffa4d21d9838421b731b4596117
Ansh Sachan <anshsachan421@gmail.com>
2026-05-07 23:25:26 +0530

    remove the db urls

--- check_db2.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const uri = process.env.MONGODB_URI || "mongodb+srv://task:manager@appmanager.haebgjj.mongodb.net/?appName=appmanager";
-
-mongoose.connect(uri).then(async () => {
-  const orgs = await mongoose.connection.collection('organizations').find({}).sort({ updatedAt: -1 }).limit(5).toArray();
-  console.log(JSON.stringify(orgs, null, 2));
-  process.exit(0);
-});
